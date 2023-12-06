884549b253d32df72b788c9f2927cb438cada48d
Ankur Goyal <ankrgyl@gmail.com>
2023-12-05 17:02:49 -0800

    Undo changes to index.ts

--- apis/cloudflare/src/index.ts
+++ apis/cloudflare/src/index.ts
@@ -1,10 +1,9 @@
-import { ResolveConfigFn, instrument } from "@microlabs/otel-cf-workers";
 import { proxyV1Prefix, handleProxyV1, handlePrometheusScrape } from "./proxy";
 export { PrometheusMetricAggregator } from "./metric-aggregator";
 
 // The fetch handler is invoked when this worker receives a HTTP(S) request
 // and should return a Response (optionally wrapped in a Promise)
-const handler = {
+export default {
   async fetch(
     request: Request,
     env: Env,
@@ -23,14 +22,3 @@ const handler = {
     });
   },
 };
-
-const config: ResolveConfigFn = (env: Env, _trigger) => {
-  return {
-    exporter: {
-      url: "http://localhost:4318/v1/traces",
-    },
-    service: { name: "greetings" },
-  };
-};
-
-export default instrument(handler, config);

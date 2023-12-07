88ce6d6b2c95e562de3a4e25ee8c689cfad62736
Ankur Goyal <ankrgyl@gmail.com>
2023-12-07 17:35:06 -0600

    Add durable objects to wrangler-template.toml

--- apis/cloudflare/src/index.ts
+++ apis/cloudflare/src/index.ts
@@ -3,11 +3,12 @@ export { PrometheusMetricAggregator } from "./metric-aggregator";
 
 // The fetch handler is invoked when this worker receives a HTTP(S) request
 // and should return a Response (optionally wrapped in a Promise)
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   async fetch(
     request: Request,
     env: Env,
-    ctx: ExecutionContext
+    ctx: ExecutionContext,
   ): Promise<Response> {
     const url = new URL(request.url);
     if (url.pathname.startsWith(proxyV1Prefix)) {

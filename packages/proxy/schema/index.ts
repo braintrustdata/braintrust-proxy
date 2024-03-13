6540cb7084a85d4947e0a7a38dc2e15b4e6a8861
Ankur Goyal <ankrgyl@gmail.com>
2024-03-13 14:59:16 -0700

    Haiku

--- packages/proxy/schema/index.ts
+++ packages/proxy/schema/index.ts
@@ -238,6 +238,7 @@ export const AvailableModels: { [name: string]: ModelSpec } = {
   "claude-instant-1.2": { format: "anthropic", flavor: "chat" },
   "claude-3-opus-20240229": { format: "anthropic", flavor: "chat" },
   "claude-3-sonnet-20240229": { format: "anthropic", flavor: "chat" },
+  "claude-3-haiku-20240307": { format: "anthropic", flavor: "chat" },
   "meta/llama-2-70b-chat": { format: "openai", flavor: "chat" },
   "llama-2-70b-chat": { format: "openai", flavor: "chat" },
   "llama-2-13b-chat": { format: "openai", flavor: "chat" },

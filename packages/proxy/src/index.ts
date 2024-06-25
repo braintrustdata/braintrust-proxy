1b08ed2144bb2785118732bc6dfc3a3df8a1da76
Ankur Goyal <ankrgyl@gmail.com>
2024-06-24 23:39:07 -0700

    Export some helpers

--- packages/proxy/src/index.ts
+++ packages/proxy/src/index.ts
@@ -2,3 +2,8 @@ export * from "./util";
 export * from "./proxy";
 export * from "./encrypt";
 export * from "./metrics";
+export {
+  parseOpenAIStream,
+  isChatCompletionChunk,
+  isCompletion,
+} from "./providers/openai";

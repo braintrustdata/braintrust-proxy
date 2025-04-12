c31e0b8463403bba24851604479c7dccd93e5064
fctb12 <fr.chalissery@gmail.com>
2025-04-11 23:34:58 -0700

    [chore] Add Gemini 2.5 Pro Preview to list of models

--- packages/proxy/schema/models.ts
+++ packages/proxy/schema/models.ts
@@ -1840,6 +1840,15 @@ export const AvailableModels: { [name: string]: ModelSpec } = {
     experimental: true,
     displayName: "Gemini 2.5 Pro Experimental",
   },
+  "gemini-2.5-pro-preview-03-25": {
+    format: "google",
+    flavor: "chat",
+    input_cost_per_mil_tokens: 1.25,
+    output_cost_per_mil_tokens: 10,
+    multimodal: true,
+    experimental: false,
+    displayName: "Gemini 2.5 Pro Preview",
+  },
   "gemini-2.0-flash-exp": {
     format: "google",
     flavor: "chat",

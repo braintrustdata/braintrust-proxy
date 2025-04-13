f50d27a26dc5da6b64e0f11e13bc051baf418375
Ankur Goyal <ankrgyl@gmail.com>
2025-04-13 13:20:42 -0700

    mostly works

--- apis/cloudflare/src/proxy.ts
+++ apis/cloudflare/src/proxy.ts
@@ -5,10 +5,31 @@ import {
   makeFetchApiSecrets,
   encryptedGet,
 } from "@braintrust/proxy/edge";
-import { NOOP_METER_PROVIDER, initMetrics } from "@braintrust/proxy";
+import {
+  NOOP_METER_PROVIDER,
+  ORG_NAME_HEADER,
+  SpanLogger,
+  initMetrics,
+  isObject,
+  parseAuthHeader,
+} from "@braintrust/proxy";
 import { PrometheusMetricAggregator } from "./metric-aggregator";
 import { handleRealtimeProxy } from "./realtime";
 import { braintrustAppUrl } from "./env";
+import {
+  Attachment,
+  BraintrustState,
+  loginToState,
+  Span,
+  startSpan,
+} from "braintrust";
+import {
+  BT_PARENT,
+  isArray,
+  SpanComponentsV3,
+  SpanObjectTypeV3,
+} from "@braintrust/core";
+import { base64ToArrayBuffer } from "@braintrust/proxy/utils";
 
 export const proxyV1Prefixes = ["/v1/proxy", "/v1"];
 
@@ -65,6 +86,30 @@ export async function handleProxyV1(
 
   const cache = await caches.open("apikey:cache");
 
+  let spanLogger: SpanLogger | undefined;
+  let span: Span | undefined;
+  const parentHeader = request.headers.get(BT_PARENT);
+  if (parentHeader) {
+    const parent = resolveParentHeader(parentHeader);
+    span = startSpan({
+      state: await loginToState({
+        apiKey:
+          parseAuthHeader({
+            authorization: request.headers.get("authorization") ?? undefined,
+          }) ?? undefined,
+        // If the app URL is explicitly set to an env var, it's meant to override
+        // the origin.
+        appUrl: braintrustAppUrl(env).toString(),
+        orgName: request.headers.get(ORG_NAME_HEADER) ?? undefined,
+        noExitFlush: true,
+      }),
+      type: "llm",
+      name: "LLM",
+      parent: parent.toStr(),
+    });
+    spanLogger = makeProxySpanLogger(span, ctx.waitUntil.bind(ctx));
+  }
+
   const opts: ProxyOpts = {
     getRelativeURL(request: Request): string {
       return new URL(request.url).pathname.slice(proxyV1Prefix.length);
@@ -114,6 +159,7 @@ export async function handleProxyV1(
     braintrustApiUrl: braintrustAppUrl(env).toString(),
     meterProvider,
     whitelist,
+    spanLogger,
   };
 
   const url = new URL(request.url);
@@ -200,4 +246,122 @@ export async function handlePrometheusScrape(
       "Content-Type": "text/plain",
     },
   });
+}
+
+export function makeProxySpanLogger(
+  span: Span,
+  waitUntil: (promise: Promise<any>) => void,
+): SpanLogger {
+  return {
+    log: (args) => {
+      span.log(replacePayloadWithAttachments(args, span.state()));
+      waitUntil(span.flush());
+    },
+    end: span.end.bind(span),
+    setName(name) {
+      span.setAttributes({ name });
+    },
+    reportProgress() {
+      return;
+    },
+  };
+}
+export function replacePayloadWithAttachments<T>(
+  data: T,
+  state: BraintrustState | undefined,
+): T {
+  return replacePayloadWithAttachmentsInner(data, state) as T;
+}
+
+function replacePayloadWithAttachmentsInner(
+  data: unknown,
+  state: BraintrustState | undefined,
+): unknown {
+  if (isArray(data)) {
+    return data.map((item) => replacePayloadWithAttachmentsInner(item, state));
+  } else if (isObject(data)) {
+    return Object.fromEntries(
+      Object.entries(data).map(([key, value]) => [
+        key,
+        replacePayloadWithAttachmentsInner(value, state),
+      ]),
+    );
+  } else if (typeof data === "string") {
+    if (isBase64Image(data)) {
+      const { mimeType, data: arrayBuffer } = getBase64Parts(data);
+      const filename = `file.${mimeType.split("/")[1]}`;
+      return new Attachment({
+        data: arrayBuffer,
+        contentType: mimeType,
+        filename,
+        state,
+      });
+    } else {
+      return data;
+    }
+  } else {
+    return data;
+  }
+}
+
+const base64ImagePattern =
+  /^data:image\/[a-zA-Z]+;base64,[A-Za-z0-9+/]+={0,2}$/;
+export function isBase64Image(s: string): boolean {
+  // Avoid unnecessary (slower) pattern matching
+  if (!s.startsWith("data:")) {
+    return false;
+  }
+
+  return base64ImagePattern.test(s);
+}
+// Being as specific as possible about allowable characters and avoiding greedy matching
+// helps avoid catastrophic backtracking: https://github.com/braintrustdata/braintrust/pull/4831
+const base64ContentTypePattern =
+  /^data:([a-zA-Z0-9]+\/[a-zA-Z0-9+.-]+);base64,/;
+export function getBase64Parts(s: string): {
+  mimeType: string;
+  data: ArrayBuffer;
+} {
+  const parts = s.match(base64ContentTypePattern);
+  if (!parts) {
+    throw new Error("Invalid base64 image");
+  }
+  const mimeType = parts[1];
+  const data = s.slice(`data:${mimeType};base64,`.length);
+  return { mimeType, data: base64ToArrayBuffer(data) };
+}
+
+// XXX delete this once the new core version is live
+const EXPERIMENT_ID_PREFIX = "experiment_id:";
+const PROJECT_ID_PREFIX = "project_id:";
+const PROJECT_NAME_PREFIX = "project_name:";
+const PLAYGROUND_ID_PREFIX = "playground_id:";
+
+export function resolveParentHeader(header: string): SpanComponentsV3 {
+  if (header.startsWith(EXPERIMENT_ID_PREFIX)) {
+    return new SpanComponentsV3({
+      object_type: SpanObjectTypeV3.EXPERIMENT,
+      object_id: header.substring(EXPERIMENT_ID_PREFIX.length),
+    });
+  } else if (header.startsWith(PROJECT_ID_PREFIX)) {
+    return new SpanComponentsV3({
+      object_type: SpanObjectTypeV3.PROJECT_LOGS,
+      object_id: header.substring(PROJECT_ID_PREFIX.length),
+    });
+  } else if (header.startsWith(PLAYGROUND_ID_PREFIX)) {
+    return new SpanComponentsV3({
+      object_type: SpanObjectTypeV3.PLAYGROUND_LOGS,
+      object_id: header.substring(PLAYGROUND_ID_PREFIX.length),
+    });
+  } else if (header.startsWith(PROJECT_NAME_PREFIX)) {
+    const projectName = header.substring(PROJECT_NAME_PREFIX.length);
+    return new SpanComponentsV3({
+      object_type: SpanObjectTypeV3.PROJECT_LOGS,
+      compute_object_metadata_args: {
+        project_name: projectName,
+      },
+    });
+  }
+
+  return SpanComponentsV3.fromStr(header);
 }

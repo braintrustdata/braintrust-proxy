e6c29744e16b6d5870cffa787fac33069b4aeb89
Ankur Goyal <ankrgyl@gmail.com>
2025-05-17 10:30:45 -0700

    move models to a json file

---
+++ packages/proxy/schema/models.test.ts
@@ -0,0 +1,16 @@
+import { expect } from "vitest";
+import { it } from "vitest";
+import raw_models from "./model_list.json";
+import { ModelSchema } from "./models";
+import { z } from "zod";
+
+it("parse model list", () => {
+  const models = z.record(z.unknown()).parse(raw_models);
+  for (const [key, value] of Object.entries(models)) {
+    const result = ModelSchema.safeParse(value);
+    if (!result.success) {
+      console.log("failed to parse ", key, result.error);
+    }
+    expect(result.success).toBe(true);
+  }
+});

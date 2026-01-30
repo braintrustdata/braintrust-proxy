a7e7043db9c3ad057df9b18e095e921406c5447e
max-braintrust <max@braintrustdata.com>
2026-01-30 15:10:00 -0800

    Add upcoming model deprecations (#379)
    - Adds optional deprecation_date field to ModelSpec so deprecation
    changes can be made ahead of the actual date on which models should be
    deprecated.
    - Adds deprecation dates for Haiku 3.5, Sonnet 3.7, gemini-2.0-flash,
    and gemini-2.0-flash-lite.
    - Updates sync_models script to also pull in deprecation_dates.
    - Updates api to mark models as deprecated if deprecation date has
    passed.
    
    ---------
    
    Co-authored-by: Max Kern <maxkern@Maxs-MBP.localdomain>
    Co-authored-by: Max Kern <maxkern@Maxs-MacBook-Pro.local>

--- packages/proxy/schema/models.test.ts
+++ packages/proxy/schema/models.test.ts
@@ -1,7 +1,7 @@
 import { expect } from "vitest";
 import { it } from "vitest";
 import raw_models from "./model_list.json";
-import { ModelSchema } from "./models";
+import { markModelsPastDeprecationDate, ModelSchema } from "./models";
 import { z } from "zod";
 
 it("parse model list", () => {
@@ -13,4 +13,65 @@ it("parse model list", () => {
     }
     expect(result.success).toBe(true);
   }
+});
+
+it("Marks models as deprecated once deprecation date has been reached", () => {
+  const result = markModelsPastDeprecationDate({
+    testModel: {
+      format: "anthropic",
+      flavor: "chat",
+      multimodal: true,
+      input_cost_per_mil_tokens: 0.8,
+      output_cost_per_mil_tokens: 4,
+      input_cache_read_cost_per_mil_tokens: 0.08,
+      input_cache_write_cost_per_mil_tokens: 1,
+      parent: "claude-3-5-haiku-latest",
+      max_input_tokens: 200000,
+      max_output_tokens: 8192,
+      deprecation_date: "2026-01-10",
+    },
+  });
+  expect(result["testModel"].deprecated).toBe(true);
+});
+
+it("Does not deprecate models if deprecation date has not yet passed", () => {
+  const now = new Date();
+  const oneYearFromNow = new Date(
+    now.setFullYear(now.getFullYear() + 1),
+  ).toDateString();
+  const result = markModelsPastDeprecationDate({
+    testModel: {
+      format: "anthropic",
+      flavor: "chat",
+      multimodal: true,
+      input_cost_per_mil_tokens: 0.8,
+      output_cost_per_mil_tokens: 4,
+      input_cache_read_cost_per_mil_tokens: 0.08,
+      input_cache_write_cost_per_mil_tokens: 1,
+      parent: "claude-3-5-haiku-latest",
+      max_input_tokens: 200000,
+      max_output_tokens: 8192,
+      deprecation_date: oneYearFromNow,
+    },
+  });
+  expect(result["testModel"].deprecated).toBe(undefined);
+});
+
+it("Ignores malformed deprecation dates", () => {
+  const result = markModelsPastDeprecationDate({
+    testModel: {
+      format: "anthropic",
+      flavor: "chat",
+      multimodal: true,
+      input_cost_per_mil_tokens: 0.8,
+      output_cost_per_mil_tokens: 4,
+      input_cache_read_cost_per_mil_tokens: 0.08,
+      input_cache_write_cost_per_mil_tokens: 1,
+      parent: "claude-3-5-haiku-latest",
+      max_input_tokens: 200000,
+      max_output_tokens: 8192,
+      deprecation_date: "not a date",
+    },
+  });
+  expect(result["testModel"].deprecated).toBe(undefined);
 });
--- packages/proxy/schema/models.ts
+++ packages/proxy/schema/models.ts
@@ -70,6 +70,10 @@ export const ModelSchema = z.object({
     .describe(
       "Discourage the use of the model (we will hide the model in the UI).",
     ),
+  deprecation_date: z
+    .string()
+    .nullish()
+    .describe("Date after which the model will be treated as deprecated"),
   parent: z.string().nullish().describe("The model was replaced this model."),
   endpoint_types: z.array(z.enum(ModelEndpointType)).nullish(),
   locations: z.array(z.string()).nullish(),
@@ -102,7 +106,9 @@ declare global {
 
 // This function will always return at least the static model list,
 export function getAvailableModels(): { [name: string]: ModelSpec } {
-  return globalThis._proxy_availableModels ?? modelListJsonTyped;
+  return markModelsPastDeprecationDate(
+    globalThis._proxy_availableModels ?? modelListJsonTyped,
+  );
 }
 
 // This function will reach out to the control plane and update the
@@ -179,4 +185,20 @@ async function loadModelsFromControlPlane(
     // Clear the promise so future requests can proceed
     _loadModelsPromise = null;
   }
+}
+
+export function markModelsPastDeprecationDate(models: {
+  [name: string]: ModelSpec;
+}): { [name: string]: ModelSpec } {
+  for (let modelName of Object.keys(models)) {
+    const { deprecation_date } = models[modelName];
+    if (
+      deprecation_date &&
+      Date.parse(deprecation_date) &&
+      new Date() > new Date(deprecation_date)
+    ) {
+      models[modelName].deprecated = true;
+    }
+  }
+  return models;
 }

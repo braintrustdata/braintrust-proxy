6129d787f928081cdeeb1fc3c83c2813392e720c
Caitlin Pinn <caitlin.pinn@gmail.com>
2026-05-14 17:43:00 -0700

    update config

--- apis/vercel/next.config.js
+++ apis/vercel/next.config.js
@@ -3,7 +3,10 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
-  outputFileTracingRoot: path.join(__dirname, "../.."),
+  outputFileTracingRoot: path.join(__dirname, "../../.."),
+  turbopack: {
+    root: path.join(__dirname, "../../.."),
+  },
   transpilePackages: ["@braintrust/proxy", "@vercel/examples-ui"],
   typescript: {
     ignoreBuildErrors: true,

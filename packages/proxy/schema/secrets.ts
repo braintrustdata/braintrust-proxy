bc320a7e8042dd086646be7a95ebf799415eabee
Olmo Maldonado <olmo@braintrust.dev>
2026-05-21 14:18:05 -0700

    add org_id

--- packages/proxy/schema/secrets.ts
+++ packages/proxy/schema/secrets.ts
@@ -154,6 +154,7 @@ export const BraintrustMetadataSchema = BaseMetadataSchema.merge(
 const APISecretBaseSchema = z
   .object({
     id: z.string().uuid().nullish(),
+    org_id: z.string().nullish(),
     org_name: z.string().nullish(),
     name: z.string().nullish(),
     secret: z.string(),
